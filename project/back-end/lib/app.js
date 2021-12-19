fb313d74db4ed91e99532aa880f70d1a7f4b27b1
mat-bh <mat-bh@github.com>
2021-12-19 18:42:25 +0100

    prettier

--- project/back-end/lib/app.js
+++ project/back-end/lib/app.js
@@ -1,176 +1,157 @@
-const db = require('./db')
-const express = require('express')
-const cors = require('cors')
-const authenticator = require('./authenticator')
+const db = require("./db");
+const express = require("express");
+const cors = require("cors");
+const authenticator = require("./authenticator");
 
-const app = express()
+const app = express();
 const authenticate = authenticator({
-  test_payload_email: process.env['TEST_PAYLOAD_EMAIL'],
-  jwks_uri: 'http://localhost:5556/dex/keys'
-})
+  test_payload_email: process.env["TEST_PAYLOAD_EMAIL"],
+  jwks_uri: "http://localhost:5556/dex/keys",
+});
 
-app.use(require('body-parser').json())
-app.use(cors())
+app.use(require("body-parser").json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send([
-    '<h1>ECE WebTech Chat</h1>'
-  ].join(''))
-})
+app.get("/", (req, res) => {
+  res.send(["<h1>ECE WebTech Chat</h1>"].join(""));
+});
 
 // Channels
 
-app.get('/channels', authenticate, async (req, res) => {
-  const channels = await db.channels.list()
-  res.json(channels)
-})
+app.get("/channels", authenticate, async (req, res) => {
+  const channels = await db.channels.list();
+  res.json(channels);
+});
 
-app.get('/channels/:users',authenticate,  async (req, res) => {
-  const channels = await db.channels.list()
-  const  result=myChannels(channels,req.params.users)
-  await isUser(req.params.users,req.headers['email'])
-  const users=await db.users.list()
-  res.json(result)
+app.get("/channels/:users", authenticate, async (req, res) => {
+  const channels = await db.channels.list();
+  const result = myChannels(channels, req.params.users);
+  await isUser(req.params.users, req.headers["email"]);
+  const users = await db.users.list();
+  res.json(result);
+});
 
-})
+app.post("/channels", authenticate, async (req, res) => {
+  const channel = await db.channels.create(req.body["channel"]);
+  res.status(201).json(channel);
+});
+app.post("/channels/init", async (req, res) => {
+  const channel = await db.channels.create(req.body["channel"]);
+  res.status(201).json(channel);
+});
 
-app.post('/channels',authenticate, async (req, res) => {
-console.log("eeeeeeeeeeeeeeeee")
-  const channel = await db.channels.create(req.body['channel'])
-  res.status(201).json(channel)
-
-})
-app.post('/channels/init', async (req, res) => {
-
-  const channel = await db.channels.create(req.body['channel'])
-  res.status(201).json(channel)
-
-})
-
-app.delete('/channels/:id/messages/:creation',authenticate, async (req, res) => {
-
-  try{
-    await db.messages.delete(req.params.id, req.params.creation)
-     res.status(200).json({ "status": true, "result": 'Edit successful!' })
+app.delete(
+  "/channels/:id/messages/:creation",
+  authenticate,
+  async (req, res) => {
+    try {
+      await db.messages.delete(req.params.id, req.params.creation);
+      res.status(200).json({ status: true, result: "Edit successful!" });
+    } catch (err) {
+      res.status(400).send({ status: false, "erreur suivante": err });
+    }
   }
-  catch(err){
-    res.status(400).send({"status":false,"erreur suivante":err})
+);
+
+app.put("/channels/:id/messages/:creation", authenticate, async (req, res) => {
+  try {
+    await db.messages.update(
+      req.body.content,
+      req.params.id,
+      req.params.creation
+    );
+    res.status(200).json({ status: true, result: "Edit successful!" });
+  } catch (err) {
+    res.status(400).send({ status: false, "erreur suivante ": err });
   }
-})
+});
+app.get("/channels/:id", authenticate, async (req, res) => {
+  const channel = await db.channels.get(req.params.id);
+  res.json(channel);
+});
 
-app.put('/channels/:id/messages/:creation',authenticate,async (req, res) => {
-  try{
+app.put("/channels/:id", authenticate, async (req, res) => {
+  await isUser(req.body["edited"].users);
 
-    await db.messages.update(req.body.content, req.params.id,req.params.creation)
-    res.status(200).json({ "status": true, "result": 'Edit successful!' })
+  try {
+    const channel = await db.channels.update(req.params.id, req.body["edited"]);
+    res.status(200).json({ status: true, result: "Edit successful!" });
+  } catch (err) {
+    res.status(400).send({ status: false, "erreur suivante": err });
   }
-  catch(err){
-    res.status(400).send({"status":false,"erreur suivante ":err})
-  }
-})
-app.get('/channels/:id', authenticate,async (req, res) => {
-  const channel = await db.channels.get(req.params.id)
-  res.json(channel)
-})
-
-app.put('/channels/:id',authenticate, async (req, res) => {
-
-  await isUser(req.body['edited'].users)
-
-    try{
-    const channel = await db.channels.update(req.params.id,req.body['edited'])
-    res.status(200).json({ "status": true, "result": 'Edit successful!' })
-  }catch(err){
-    res.status(400).send({"status":false,"erreur suivante":err})
-  }
-
-})
-
+});
 
 // Messages
 
-app.get('/channels/:id/messages',authenticate, async (req, res) => {
-
-  try{
-    const channel = await db.channels.get(req.params.id)
-  }catch(err){
-    return res.status(404).send('Channel does not exist.')
+app.get("/channels/:id/messages", authenticate, async (req, res) => {
+  try {
+    const channel = await db.channels.get(req.params.id);
+  } catch (err) {
+    return res.status(404).send("Channel does not exist.");
   }
 
-  const messages = await db.messages.list(req.params.id)
-  res.json(messages)
+  const messages = await db.messages.list(req.params.id);
+  res.json(messages);
+});
 
-})
-
-app.post('/channels/:id/messages',authenticate, async (req, res) => {
-  const message = await db.messages.create(req.params.id, req.body)
-  res.status(201).json(message)
-})
+app.post("/channels/:id/messages", authenticate, async (req, res) => {
+  const message = await db.messages.create(req.params.id, req.body);
+  res.status(201).json(message);
+});
 
 // Users
 
-app.get('/users',authenticate, async (req, res) => {
-  const users = await db.users.list()
-  res.json(users)
-})
+app.get("/users", authenticate, async (req, res) => {
+  const users = await db.users.list();
+  res.json(users);
+});
 
-app.post('/users',  isUser)
+app.post("/users", isUser);
 
+app.get("/users/:username", authenticate, async (req, res) => {
+  const users = await db.users.list();
+  const user = await db.users.get(req.params.username);
+  res.json(user);
+});
 
-app.get('/users/:username',authenticate, async (req, res) => {
-  const users=await db.users.list()
-  const user = await db.users.get(req.params.username)
-  res.json(user)
-})
-
-app.put('/users/:id',authenticate, async (req, res) => {
-  try{
-    const user = await db.users.update(req.body['user'])
-    res.status(200).json({ "status": true, "result": 'Edit successful!' })
+app.put("/users/:id", authenticate, async (req, res) => {
+  try {
+    const user = await db.users.update(req.body["user"]);
+    res.status(200).json({ status: true, result: "Edit successful!" });
+  } catch (err) {
+    res.status(400).send({ status: false, "erreur suivante": err });
   }
-  catch(err){
-      res.status(400).send({"status":false,"erreur suivante":err})
+});
+
+async function isUser(username, email) {
+  const bddUsers = await db.users.list();
+  if (!bddUsers.find((user) => user.username === username)) {
+    const user = {
+      username: username,
+      email: email,
+      language: "français",
+      nationalitie: "français",
+      avatar: "/broken-image.jpg",
+    };
+    await db.users.create(user);
+    return false;
+  } else {
+    return true;
   }
-
-})
-
-async function isUser(username,email)
-{
-
- const bddUsers=await db.users.list()
-   if(!bddUsers.find(user=>user.username===username))
-   {
-     const user={
-
-       username:username,
-       email:email,
-       language:'français',
-       nationalitie:'français',
-       avatar :'/broken-image.jpg',
-     }
-     await db.users.create(user)
-     return false
-   }
-   else{
-     return true
-   }
 }
 
- function  myChannels(channels,user)
-{
-  let result=[]
-   for(let i=0; i<channels.length;i++)
-   {
-     if(channels[i].users != undefined)
-    {for( let l=0; l<channels[i].users.length;l++)
-     {
-       if(channels[i].users[l] ===user)
-       {
-        result.push(channels[i])
-       }
-     }}
-   }
-   return result
+function myChannels(channels, user) {
+  let result = [];
+  for (let i = 0; i < channels.length; i++) {
+    if (channels[i].users != undefined) {
+      for (let l = 0; l < channels[i].users.length; l++) {
+        if (channels[i].users[l] === user) {
+          result.push(channels[i]);
+        }
+      }
+    }
+  }
+  return result;
 }
 
-module.exports = app
+module.exports = app;
--- project/back-end/lib/db.js
+++ project/back-end/lib/db.js
@@ -1,138 +1,155 @@
-
-const {v4: uuid} = require('uuid')
-const {clone, merge} = require('mixme')
-const microtime = require('microtime')
-const level = require('level')
-const db = level(__dirname + '/../db')
+const { v4: uuid } = require("uuid");
+const { clone, merge } = require("mixme");
+const microtime = require("microtime");
+const level = require("level");
+const db = level(__dirname + "/../db");
 
 module.exports = {
   channels: {
     create: async (channel) => {
-      if(!channel.name) throw Error('Invalid channel')
-      channel.id = uuid()
-      await db.put(`channels:${channel.id}`, JSON.stringify(channel))
-      const data = await db.get(`channels:${channel.id}`)
-      return merge(channel, {id: channel.id})
+      if (!channel.name) throw Error("Invalid channel");
+      channel.id = uuid();
+      await db.put(`channels:${channel.id}`, JSON.stringify(channel));
+      const data = await db.get(`channels:${channel.id}`);
+      return merge(channel, { id: channel.id });
     },
     get: async (id) => {
-      if(!id) throw Error('Invalid id')
-      const data = await db.get(`channels:${id}`)
-      const channel = JSON.parse(data)
-      return merge(channel, {id: id})
+      if (!id) throw Error("Invalid id");
+      const data = await db.get(`channels:${id}`);
+      const channel = JSON.parse(data);
+      return merge(channel, { id: id });
     },
     list: async () => {
-      return new Promise( (resolve, reject) => {
-        const channels = []
+      return new Promise((resolve, reject) => {
+        const channels = [];
         db.createReadStream({
           gt: "channels:",
           lte: "channels" + String.fromCharCode(":".charCodeAt(0) + 1),
-        }).on( 'data', ({key, value}) => {
-          channel = JSON.parse(value)
-          channel.id = key.split(':')[1]
-          channels.push(channel)
-        }).on( 'error', (err) => {
-          reject(err)
-        }).on( 'end', () => {
-          resolve(channels)
         })
-      })
+          .on("data", ({ key, value }) => {
+            channel = JSON.parse(value);
+            channel.id = key.split(":")[1];
+            channels.push(channel);
+          })
+          .on("error", (err) => {
+            reject(err);
+          })
+          .on("end", () => {
+            resolve(channels);
+          });
+      });
     },
-    update: async(id, editedChannel) => {
-      const data = await db.put(`channels:${id}`,JSON.stringify(editedChannel))
-
+    update: async (id, editedChannel) => {
+      const data = await db.put(
+        `channels:${id}`,
+        JSON.stringify(editedChannel)
+      );
     },
     delete: (id, channel) => {
-      const original = store.channels[id]
-      if(!original) throw Error('Unregistered channel id')
-      delete store.channels[id]
-    }
+      const original = store.channels[id];
+      if (!original) throw Error("Unregistered channel id");
+      delete store.channels[id];
+    },
   },
   messages: {
     create: async (channelId, message) => {
-      if(!channelId) throw Error('Invalid channel')
-      if(!message.author) throw Error('Invalid message')
-      if(!message.content) throw Error('Invalid message')
-      creation = microtime.now()
-      await db.put(`messages:${channelId}:${creation}`, JSON.stringify({
-        author: message.author,
-        content: message.content
-      }))
-      return merge(message, {channelId: channelId, creation: creation})
+      if (!channelId) throw Error("Invalid channel");
+      if (!message.author) throw Error("Invalid message");
+      if (!message.content) throw Error("Invalid message");
+      creation = microtime.now();
+      await db.put(
+        `messages:${channelId}:${creation}`,
+        JSON.stringify({
+          author: message.author,
+          content: message.content,
+        })
+      );
+      return merge(message, { channelId: channelId, creation: creation });
     },
-     delete: async (channelId, creation) => {
-
-       await db.del(`messages:${channelId}:${creation}`)
-      return {sucess: true}
-  },
-     update: async(content,channelId,creation)=>{
-       const editMessage=content
-       const data=await db.get(`messages:${channelId}:${creation}`)
-       let message=JSON.parse(data)
-       message.content=editMessage
-       await db.put(`messages:${channelId}:${creation}`,JSON.stringify(message))
-     },
+    delete: async (channelId, creation) => {
+      await db.del(`messages:${channelId}:${creation}`);
+      return { sucess: true };
+    },
+    update: async (content, channelId, creation) => {
+      const editMessage = content;
+      const data = await db.get(`messages:${channelId}:${creation}`);
+      let message = JSON.parse(data);
+      message.content = editMessage;
+      await db.put(
+        `messages:${channelId}:${creation}`,
+        JSON.stringify(message)
+      );
+    },
     list: async (channelId) => {
-      return new Promise( (resolve, reject) => {
-        const messages = []
+      return new Promise((resolve, reject) => {
+        const messages = [];
         db.createReadStream({
           gt: `messages:${channelId}:`,
-          lte: `messages:${channelId}` + String.fromCharCode(":".charCodeAt(0) + 1),
-        }).on( 'data', ({key, value}) => {
-          message = JSON.parse(value)
-          const [, channelId, creation] = key.split(':')
-          message.channelId = channelId
-          message.creation = creation
-          messages.push(message)
-        }).on( 'error', (err) => {
-          reject(err)
-        }).on( 'end', () => {
-          resolve(messages)
+          lte:
+            `messages:${channelId}` +
+            String.fromCharCode(":".charCodeAt(0) + 1),
         })
-      })
+          .on("data", ({ key, value }) => {
+            message = JSON.parse(value);
+            const [, channelId, creation] = key.split(":");
+            message.channelId = channelId;
+            message.creation = creation;
+            messages.push(message);
+          })
+          .on("error", (err) => {
+            reject(err);
+          })
+          .on("end", () => {
+            resolve(messages);
+          });
+      });
     },
   },
   users: {
     create: async (user) => {
-      if(!user.username) throw Error('Invalid user')
+      if (!user.username) throw Error("Invalid user");
       //const id = uuid()
-      await db.put(`users:${user.username}`, JSON.stringify(user))
-      return merge(user, {id: user.username})
+      await db.put(`users:${user.username}`, JSON.stringify(user));
+      return merge(user, { id: user.username });
     },
     get: async (username) => {
-      if(!username) throw Error('Invalid username')
-      const data = await db.get(`users:${username}`)
-      const user = JSON.parse(data)
-      return merge(user, {id: username})
+      if (!username) throw Error("Invalid username");
+      const data = await db.get(`users:${username}`);
+      const user = JSON.parse(data);
+      return merge(user, { id: username });
     },
     list: async () => {
-      return new Promise( (resolve, reject) => {
-        const users = []
+      return new Promise((resolve, reject) => {
+        const users = [];
         db.createReadStream({
           gt: "users:",
           lte: "users" + String.fromCharCode(":".charCodeAt(0) + 1),
-        }).on( 'data', ({key, value}) => {
-          user = JSON.parse(value)
-          user.id = key.split(':')[1]
-          users.push(user)
-        }).on( 'error', (err) => {
-          reject(err)
-        }).on( 'end', () => {
-          resolve(users)
         })
-      })
+          .on("data", ({ key, value }) => {
+            user = JSON.parse(value);
+            user.id = key.split(":")[1];
+            users.push(user);
+          })
+          .on("error", (err) => {
+            reject(err);
+          })
+          .on("end", () => {
+            resolve(users);
+          });
+      });
     },
     update: async (user) => {
-      await db.put(`users:${user.username}`,JSON.stringify(user))
+      await db.put(`users:${user.username}`, JSON.stringify(user));
     },
     delete: (id, user) => {
-      const original = store.users[id]
-      if(!original) throw Error('Unregistered user id')
-      delete store.users[id]
-    }
+      const original = store.users[id];
+      if (!original) throw Error("Unregistered user id");
+      delete store.users[id];
+    },
   },
   admin: {
     clear: async () => {
-      await db.clear()
-    }
-  }
-}
+      await db.clear();
+    },
+  },
+};
